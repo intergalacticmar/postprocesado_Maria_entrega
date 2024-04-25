7b8fe40287b41dbad0810a9098b5ef4237e6e5bd
María <120553496+intergalacticmar@users.noreply.github.com>
2024-04-25 12:39:41 +0200

    guardado final

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody rb;
+
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
+        transform.Translate(movement);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
